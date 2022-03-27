2599afd0586cc3621170982cbeeb502b6c490589
Marco Aurelio Avila <marcoaurelio_dev@outlook.com>
2022-03-26 22:18:49 -0300

    app

--- scripts.js
+++ scripts.js
@@ -14,45 +14,50 @@ const Modal = {
   }
 }
 
-const transactions = [
+
+
+const Transaction = {
+  all: [
     {
-      id: 1,
+      
     description: 'Luz', 
     amount: -50001,
     date: '30/07/2021',
     },
 
     {
-      id: 2,
+   
     description: 'Criação de Web site', 
     amount: 500000,
     date: '30/07/2021',
     },
 
     {
-      id: 3,
+   
     description: 'Internet', 
     amount: -20012,
     date: '30/07/2021',
     },
 
     {
-      id: 4,
+  
     description: 'App', 
     amount: 200000,
     date: '30/07/2021',
     }
-]
-
-const Transaction = {
-  all: transactions,
+],
 
   add(transaction) {
 
     Transaction.all.push(transaction)
-    console.log(Transaction.all);
-
+    App.reload()
   },
+
+  remove(index) {
+  Transaction.all.splice(index, 1)
+  App.reload()
+  },
+
   incomes () {
     let income = 0;
     // pegar todas as transações
@@ -144,6 +149,10 @@ updateBalance (){
   .getElementById('totalDisplay')
   .innerHTML = Utils.formatCurrent(Transaction.total())
   
+},
+
+clearTransactions(){
+DOM.transactionsContainer.innerHTML = ""
 }
 
 }
@@ -163,16 +172,28 @@ const Utils = {
   }
 }
 
-transactions.forEach(function(transaction) {
-  DOM.addTransaction(transaction)
-})
+const Form = {
 
-DOM.updateBalance()
+}
+
+const App = {
+  init() {
+
+    Transaction.all.forEach(transaction => {
+      DOM.addTransaction(transaction)
+    })
+    
+    DOM.updateBalance()
+    
+   
+    
+  },
+  reload(){
+    DOM.clearTransactions()
+  App.init()
+  }, 
+}
+
+App.init()
 
 
-Transaction.add({
-  id:39,
-  description: 'alo',
-  amount: 200,
-  date: '25/03/2022'
-})
